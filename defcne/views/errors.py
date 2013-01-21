fe9357eb32e99bd5771ec47a1e189716acfe9857
Bert JW Regeer <bertjw@regeer.org>
2013-01-20 22:48:50 -0700

    Fix missing view callables
    This adds the view callables that are required to exist, so that upon
    startup Pyramid doesn't complain about them missing.
    
    More work has to be done on these.

--- defcne/views/errors.py
+++ defcne/views/errors.py
@@ -2,4 +2,5 @@
 # Author: Bert JW Regeer <bertjw@regeer.org>
 # Created: 2013-01-05
 
-
+def not_found(request):
+    return {}
--- defcne/views/home.py
+++ defcne/views/home.py
@@ -2,4 +2,5 @@
 # Author: Bert JW Regeer <bertjw@regeer.org>
 # Created: 2013-01-05
 
-
+def home(request):
+    return {}
