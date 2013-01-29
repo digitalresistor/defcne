8e1b2fd2c10ba46a070ad9496fe0707b2cfb6f6c
Bert JW Regeer <bertjw@regeer.org>
2013-01-28 21:40:38 -0700

    Add convenience function/email field to User model
    This adds the convenience function find_user() to the User model, which
    builds the right query to see if a user exists in the system.

--- defcne/models/User.py
+++ defcne/models/User.py
@@ -3,10 +3,12 @@
 # Created: 2013-01-05
 
 from meta import Base
+from meta import DBSession
 
 from sqlalchemy import (
         Boolean,
         Column,
+        ForeignKey,
         Integer,
         String,
         Table,
@@ -22,11 +24,16 @@ class User(Base):
             Column('id', Integer, primary_key=True, unique=True),
             Column('username', Unicode(128), unique=True, index=True),
             Column('realname', Unicode(256), index=True),
+            Column('email', Unicode(256), unique=True),
             Column('credentials', Unicode(60)), # bcrypt
             Column('validated', Boolean, default=False),
             )
 
     groups = relationship("Group", secondary="user_groups", lazy="joined")
+
+    @classmethod
+    def find_user(cls, username):
+        return DBSession.query(cls).filter(cls.username == username).first()
 
 class UserValidation(Base):
     __table__ = Table('user_validation', Base.metadata,
