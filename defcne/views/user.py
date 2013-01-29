c6927bdafb7459a5adb667c7052a307d1f891043
Bert JW Regeer <bertjw@regeer.org>
2013-01-28 22:47:43 -0700

    Upon submitting form create new user/validation token
    Once the user form has been submitted we create the new user, and a new
    validation token (uuid), we then make sure that both are properly in the
    database, and send the user to the validation page.

--- defcne/views/user.py
+++ defcne/views/user.py
@@ -2,8 +2,12 @@
 # Author: Bert JW Regeer <bertjw@regeer.org>
 # Created: 2013-01-20
 
+from uuid import uuid4
+
+import transaction
 from pyramid.httpexceptions import HTTPFound
 
+from sqlalchemy.exc import IntegrityError
 from deform import (Form, ValidationFailure)
 
 from ..forms import UserForm
@@ -34,7 +38,27 @@ class User(object):
             # Add the user to the database
             user = m.User(username=appstruct['username'], realname=appstruct['realname'], email=appstruct['email'], credentials=appstruct['password'])
             m.DBSession.add(user)
-            m.DBSession.flush()
+            try:
+                m.DBSession.flush()
+            except IntegrityError, e:
+                return HTTPFound(location = self.request.current_route_url())
+
+            # Create new validation token
+
+            sp = transaction.savepoint()
+            while 1:
+                try:
+                    uservalidation = m.UserValidation(user_id=user.id, token=unicode(uuid4()))
+                    m.DBSession.add(uservalidation)
+                    m.DBSession.flush()
+                    break
+                except IntegrityError, e:
+                    sp.rollback()
+                    continue
+
+            print ("Created a new user \"{user}\" with token \"{token}\"".format(user=user.username, token=uservalidation.token))
+            # Send out validation email to email address on for user
+
             # Redirect user to waiting on validation
             return HTTPFound(location = self.request.route_url('defcne.user.create.validation'))
         except ValidationFailure, e:
