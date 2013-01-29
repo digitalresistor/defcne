a34eaa6932905ef987afd8a81b26347cb784bdc7
Bert JW Regeer <bertjw@regeer.org>
2013-01-28 22:45:58 -0700

    Verify email address provided is unique
    This adds another validator to the email field that verifies that the
    email address is unique in the database.

--- defcne/forms/User.py
+++ defcne/forms/User.py
@@ -13,11 +13,15 @@ def validate_unique_username(node, value):
     if User.find_user(value) != None:
         raise colander.Invalid(node, msg='Username already exists.')
 
+def validate_unique_email(node, value):
+    if User.find_user_by_email(value) != None:
+        raise colander.Invalid(node, msg='Email has previously been registered.')
+
 class UserForm(CSRFSchema):
     """The user registration form."""
     username = colander.SchemaNode(colander.String(), title="Username", validator=validate_unique_username)
     realname = colander.SchemaNode(colander.String(), title="Name")
-    email    = colander.SchemaNode(colander.String(), title="Email address", validator=colander.Length(max=254))
+    email    = colander.SchemaNode(colander.String(), title="Email address", validator=colander.All(colander.Length(max=254), validate_unique_email))
     password = colander.SchemaNode(
                 colander.String(),
                 validator=colander.Length(min=5),
