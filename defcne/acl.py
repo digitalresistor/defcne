4974f7d6d5593e71248d5b67a498dcc58d8ace65
Bert JW Regeer <bertjw@regeer.org>
2013-02-09 15:28:49 -0700

    Change the User ACL factory
    We really only need to define various ACL's, to allow editing/viewing,
    and then we use those on the views.
    
    Also change it so that __getitem__ always returns KeyError.

--- defcne/acl.py
+++ defcne/acl.py
@@ -11,30 +11,13 @@ from pyramid.security import (
         )
 
 class User(object):
-    permissions = {
-            'edit': [
+    __acl__ = [
+                (Allow, Authenticated, 'view'),
                 (Allow, Authenticated, 'edit'),
-                ],
-            'complete': [
-                (Allow, Authenticated, 'view'),
-                ],
-            }
-
-    @property
-    def __acl__(self):
-        if self.key is not None:
-            return self.permissions[self.key]
-        return [
-                (Allow, Authenticated, 'view'),
-                ]
+              ]
 
     def __init__(self, request):
-        self.key = None
+        pass
 
     def __getitem__(self, key):
-        # We use the key if available to set the permissions for the context,
-        # but besides that we don't use it!
-        if key in self.permissions:
-            self.key = key
-        
         raise KeyError
