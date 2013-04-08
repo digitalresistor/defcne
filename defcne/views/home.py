968e51c0be72576820151f43acc482c11ab0dff8
Bert JW Regeer <bertjw@regeer.org>
2013-04-07 22:20:38 -0600

    Remove unnecessary code

--- defcne/views/home.py
+++ defcne/views/home.py
@@ -2,7 +2,6 @@
 # Author: Bert JW Regeer <bertjw@regeer.org>
 # Created: 2013-01-05
 
-from pyramid.security import authenticated_userid
-
 def home(request):
-    return {'user': authenticated_userid(request)}
+    return {}
+
