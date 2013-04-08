86a1482ea77d8aeb7cf06105fca6ea7af50e9b24
Bert JW Regeer <bertjw@regeer.org>
2013-04-08 04:37:30 -0600

    user_forgotpassword subscriber now sends email
    The subscriber now sends an email with a reset link to the user.

--- defcne/subscribers/__init__.py
+++ defcne/subscribers/__init__.py
@@ -25,8 +25,30 @@ def user_created(event):
     message = Message(subject="DEFCnE User Account Validation", sender="defcne@defcne.net", recipients=[event.user.email], body=text)
     get_mailer(event.request).send(message)
 
+
+__user_forgotpassword__ = """DEFCnE Reset Account
+
+You, or someone claiming to be you has requested to reset the password on your
+account. If you did not request a password reset you may safely ignore this
+email, otherwise the link below will take you to the password reset:
+
+{reset_url}
+
+If you can't click the link, you can use the following information instead:
+
+url: {reset_url_nodata}
+username: {username}
+token: {token}
+
+Thanks,
+DEFCnE Team"""
+
 def user_forgotpassword(event):
-    pass
+    # Sent the user an email with the reset link
+
+    text = __user_forgotpassword__.format(reset_url=event.kw['reset_url'], reset_url_nodata=event.request.route_url('defcne.user', traverse=('reset')), username=event.user.disp_uname, token=event.kw['token'])
+    message = Message(subject="DEFCnE Reset Account", sender="defcne@defcne.net", recipients=[event.user.email], body=text)
+    get_mailer(event.request).send(message)
 
 def user_passwordupdated(event):
     pass
