af80c610107a32cb2e17d1bb217b09569b5f8880
Bert JW Regeer <bertjw@regeer.org>
2013-04-08 03:46:12 -0600

    Make sure to inherit from the right base

--- defcne/events/__init__.py
+++ defcne/events/__init__.py
@@ -26,7 +26,7 @@ class ContestEventEvents(object):
 class ContestEventCreated(ContestEventEvents):
     pass
 
-class ContestEventUpdated(object):
+class ContestEventUpdated(ContestEventEvents):
     pass
 
 
