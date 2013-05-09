aa6547a9588254ae3c78d61249e18ef5a2a0e00e
Bert JW Regeer <bertjw@regeer.org>
2013-05-09 00:40:43 -0600

    Basic Magic view

---
+++ defcne/views/magic.py
@@ -0,0 +1,44 @@
+# File: magic.py
+# Author: Bert JW Regeer <bertjw@regeer.org>
+# Created: 2013-04-01
+
+import logging
+log = logging.getLogger(__name__)
+
+from uuid import uuid4
+
+from pyramid.security import authenticated_userid
+from pyramid.httpexceptions import HTTPSeeOther, HTTPNotFound
+
+import transaction
+
+from sqlalchemy.exc import IntegrityError
+from deform import (Form, ValidationFailure)
+
+#from ..forms import (
+#        )
+
+from .. import models as m
+
+class Magic(object):
+    """View for Magic functionality"""
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def main(self):
+        return {}
+
+    def dcyears(self):
+        return {}
+
+    def dcevents(self):
+        return {}
+
+    def users(self):
+        return {}
+
+    def user(self):
+        return {}
+
