1704c06b83efbe9e26e3087190458aeb0b5d0b4c
Bert JW Regeer <bertjw@regeer.org>
2014-03-21 13:24:01 -0600

    Update CSRF base form

--- defcne/forms/csrf.py
+++ defcne/forms/csrf.py
@@ -2,25 +2,45 @@
 # Author: Bert JW Regeer <bertjw@regeer.org>
 # Created: 2013-01-26
 
+import logging
+log = logging.getLogger(__name__)
+
+from pyramid.i18n import TranslationStringFactory
+_ = TranslationStringFactory('usingnamespace')
+
 import colander
 import deform
 
 @colander.deferred
 def deferred_csrf_default(node, kw):
     request = kw.get('request')
+
+    if request is None:
+        raise KeyError('Require bind: request')
+
     csrf_token = request.session.get_csrf_token()
     return csrf_token
 
 @colander.deferred
 def deferred_csrf_validator(node, kw):
+    request = kw.get('request')
+
+    if request is None:
+        raise KeyError('Require bind: request')
+
     def validate_csrf(node, value):
-        request = kw.get('request')
         csrf_token = request.session.get_csrf_token()
         if value != csrf_token:
-            raise ValueError('Bad CSRF token')
+            log.error('CSRF Failed: expected "{}" got "{}"'.format(csrf_token,
+                value))
+            exc = colander.Invalid(node, _('Invalid cross-site scripting token. Please submit your form again.'))
+            raise exc
     return validate_csrf
 
 class CSRFSchema(colander.Schema):
-    csrf_token = colander.SchemaNode(colander.String(),
-            default=deferred_csrf_default, validator=deferred_csrf_validator,
-            widget=deform.widget.HiddenWidget(),)
+    csrf_token = colander.SchemaNode(
+            colander.String(),
+            default=deferred_csrf_default,
+            validator=deferred_csrf_validator,
+            widget=deform.widget.HiddenWidget()
+            )
