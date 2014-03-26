c42b3fd4c6adb687f458195e5d257651a21fe28a
Bert JW Regeer <bertjw@regeer.org>
2014-03-26 00:00:24 -0600

    Clean up the Event model

--- defcne/models/event.py
+++ defcne/models/event.py
@@ -31,44 +31,6 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from cvebase import CVEBase
 
 
-class Event(CVEBase):
-    __table__ = Table('events', Base.metadata,
-            Column('id', Integer, ForeignKey('cve.id', onupdate="CASCADE", ondelete="CASCADE"), primary_key=True),
-            Column('onsite', Boolean),
-            Column('official', Boolean),
-            Column('security', Boolean),
-            Column('signage', UnicodeText),
-            )
-    __mapper_args__ = {
-                'polymorphic_identity': 'event',
-            }
-
-    space = relationship("EventSpace")
-
-    def from_appstruct(self, appstruct):
-        super(Event, self).from_appstruct(appstruct)
-
-        self.onsite = appstruct['onsite']
-        self.official = appstruct['official']
-        self.security = appstruct['security']
-        self.signage = appstruct['signage']
-
-    def to_appstruct(self):
-        ret = super(Event, self).to_appstruct()
-
-        ret.update(
-                    {
-                        'id': self.id,
-                        'onsite': self.onsite,
-                        'official': self.official,
-                        'security': self.security,
-                        'signage': self.signage,
-                    }
-                )
-
-        return ret
-
-
 class EventSpace(Base):
     __table__ = Table('event_space', Base.metadata,
             Column('event_id', Integer, ForeignKey('events.id', onupdate="CASCADE", ondelete="CASCADE"), primary_key=True),
@@ -80,7 +42,6 @@ class EventSpace(Base):
             )
 
     def from_appstruct(self, appstruct):
-        self.event_id = appstruct['event_id']
         self.tables = appstruct['tables']
         self.chairs = appstruct['chairs']
         self.stage = appstruct['stage']
@@ -96,3 +57,52 @@ class EventSpace(Base):
                 'location': self.location,
                 'mobilebar': self.mobilebar,
                 }
+
+
+class Event(CVEBase):
+    __table__ = Table('events', Base.metadata,
+            Column('id', Integer, ForeignKey('cve.id', onupdate="CASCADE", ondelete="CASCADE"), primary_key=True),
+            Column('onsite', Boolean),
+            Column('official', Boolean),
+            Column('security', Boolean),
+            Column('signage', UnicodeText),
+            )
+    __mapper_args__ = {
+                'polymorphic_identity': 'event',
+            }
+
+    space = relationship("EventSpace", uselist=False)
+
+    def from_appstruct(self, appstruct):
+        super(Event, self).from_appstruct(appstruct)
+
+        self.onsite = appstruct['onsite']
+        self.official = appstruct['official']
+        self.security = appstruct['security']
+        self.signage = appstruct['signage']
+
+        if self.onsite and self.space is None:
+            self.space = EventSpace()
+            self.space.from_appstruct(appstruct['space'])
+
+        if self.onsite and self.space:
+            self.space.from_appstruct(appstruct['space'])
+
+    def to_appstruct(self):
+        ret = super(Event, self).to_appstruct()
+
+        ret.update(
+                    {
+                        'id': self.id,
+                        'onsite': self.onsite,
+                        'official': self.official,
+                        'security': self.security,
+                        'signage': self.signage,
+                    }
+                )
+
+        if self.space is not None:
+            ret.update({'space': self.space.to_appstruct()})
+
+        return ret
+
