5461ac1986432321427388124d8c958eeb1e9bfe
Bert JW Regeer <bertjw@regeer.org>
2014-03-30 16:54:24 -0600

    Version 0.0.4

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ development = requires + [
     ]
 
 setup(name='defcne',
-      version='0.0.3',
+      version='0.0.4',
       description='defcne',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
